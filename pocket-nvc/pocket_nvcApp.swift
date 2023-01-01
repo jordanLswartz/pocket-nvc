0d533341057d8e0be0635914e2eac729f50d3a7a
Jordan Swartz <jordanlswartz@gmail.com>
2023-01-01 10:51:05 -0800

    Initial Commit

---
+++ pocket-nvc/pocket_nvcApp.swift
@@ -0,0 +1,17 @@
+//
+//  pocket_nvcApp.swift
+//  pocket-nvc
+//
+//  Created by Jordan Swartz on 1/1/23.
+//
+
+import SwiftUI
+
+@main
+struct pocket_nvcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
