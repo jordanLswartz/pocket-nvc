77f97052d9564e8150c47f2f7a40441f37c50e08
Jordan Swartz <jordanlswartz@gmail.com>
2023-01-10 19:25:52 -0800

    completed through chapter 7

--- pocket-nvc/FeelingsView.swift
+++ pocket-nvc/FeelingsView.swift
@@ -1,20 +1,25 @@
 //
-//  FeelingsView.swift
+//  TestView.swift
 //  pocket-nvc
 //
 //  Created by Jordan Swartz on 1/1/23.
 //
 
 import SwiftUI
+import PDFKit
 
 struct FeelingsView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
 
-struct FeelingsView_Previews: PreviewProvider {
-    static var previews: some View {
-        FeelingsView()
+    let pdfDoc: PDFDocument
+
+    init() {
+        //for the sake of example, we're going to assume
+        //you have a file Lipsum.pdf in your bundle
+        let url = Bundle.main.url(forResource: "feelings", withExtension: "pdf")!
+        pdfDoc = PDFDocument(url: url)!
+    }
+
+    var body: some View {
+        PDFKitView(showing: pdfDoc)
     }
 }
